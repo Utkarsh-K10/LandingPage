80b5012f496a163c059f8db0ed5438c580792d3f
Utkarsh-K10 <utkarshkushwah9@gmail.com>
2023-04-28 17:49:55 +0530

    navbar degin

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,18 +1,31 @@
 import * as React from "react"
-import { AppBar, Tab, Tabs, Toolbar, Typography } from "@mui/material"
-import { DrawerComp } from "./DrawerComp"
+import { AppBar, Box, Link, Tab, Tabs, Toolbar, Typography} from "@mui/material"
+// import { DrawerComp } from "./DrawerComp"
+import logo from "../src/assets/rkmslogo2.png"
+
 export const Header = () => {
     return (
         <React.Fragment>
-            <AppBar>
+            <AppBar sx={{background:"#ea80fc"}}>
                 <Toolbar>
-                    <Typography>Navbar</Typography>
-                    <Tabs>
-                        <Tab label="About US"/>
-                        <Tab label="Admission"/>
-                    </Tabs>
+                    <Link href="/">
+                        <Box
+                            component={"img"}
+                            sx={{ height: 96 }}
+                            alt="logo"
+                            src={logo}
+                        />
+                    </Link>
+                    <Typography variant="h4" fontFamily={"revert"} fontWeight={10} fontSize={45} color={"#4a148c"} alignItems={"center"} margin={"auto"}> R K Memorial Hr. Sec. School</Typography>
+                    <>
+                        <Tabs sx={{marginLeft:"auto"}}>
+                            <Tab label="About US" />
+                            <Tab label="Admission" />
+                            <Tab label="Gallery" />
+                            <Tab label="Facilities"/>
+                        </Tabs>
+                    </>
                 </Toolbar>
-            <DrawerComp/>
             </AppBar>
         </React.Fragment>
     )
