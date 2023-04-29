4173ac932fad1368d66c375dabcabda547b3b633
Utkarsh-K10 <utkarshkushwah9@gmail.com>
2023-04-30 00:22:30 +0530

    drawer addition

--- src/DrawerComp.tsx
+++ src/DrawerComp.tsx
@@ -1,5 +1,26 @@
-export const DrawerComp = () => {
+import React, { useState } from "react"
+import { Drawer, IconButton, List, ListItemButton, ListItemIcon, ListItemText} from "@mui/material"
+import MenuIcon from '@mui/icons-material/Menu';
+
+export const DrawerComp:React.FC = () => {
+  const [opendrawer, setOpendrawer] = useState(false);
   return (
-    <div>DrawerComp</div>
+    <React.Fragment>
+      <Drawer open={opendrawer} onClose={()=>setOpendrawer(false)} >
+        <List>
+          <ListItemButton>
+            <ListItemIcon>
+              <ListItemText>About Us</ListItemText>
+              <ListItemText>Admission Enquiry</ListItemText>
+              <ListItemText>Pay Fee</ListItemText>
+              <ListItemText>FAQs</ListItemText>
+            </ListItemIcon>
+          </ListItemButton>
+        </List>
+      </Drawer>
+      <IconButton onClick={()=>setOpendrawer(!opendrawer)} sx={{color:"orangered", marginLeft:"auto"}}>
+        <MenuIcon/>
+      </IconButton>
+    </React.Fragment>
   )
 }
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,10 +1,21 @@
 import * as React from "react"
-import { AppBar, Box, Link, Tab, Tabs, Toolbar, Typography } from "@mui/material"
+import { AppBar, Box, Link, Tab, Tabs, Toolbar, Typography, useMediaQuery, useTheme } from "@mui/material"
 import logo from "../src/assets/rkmslogo2.png"
 import Carousal from "./Carousal"
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import PhoneInTalkIcon from '@mui/icons-material/PhoneInTalk';
-export const Header = () => {
+import { DrawerComp } from "./DrawerComp";
+
+interface stateProp {
+    value?:number
+}
+
+export const Header:React.FC = () => {
+
+    const [value, setValue] = React.useState<stateProp>({value:0})
+    const theme = useTheme();
+    const isMatch = useMediaQuery(theme.breakpoints.down("md"))
+
     return (
         <React.Fragment>
             <AppBar sx={{ background: "#f1f8e9" }}>
@@ -18,30 +29,39 @@ export const Header = () => {
                             alignItems={"center"}
                         />
                     </Link>
-                    <Typography
-                        marginLeft="5px"
-                        variant="h5"
-                        fontFamily={"revert"}
-                        color={"#121858"}
-                        alignItems={"center"}
-                    >
-                        R K Memorial Hr. Sec. School
-                        <Typography variant="caption" display="block">
-                            DISE Code: 312241   Reg. No:1875
-                        </Typography>
-                    </Typography>
-                    <MailOutlineIcon color="warning" sx={{ marginLeft: "auto" }} />
-                    <Typography variant="h6" fontWeight={70} color={"orangered"}> rkmemorialhss@gmail.com</Typography>
-                    <PhoneInTalkIcon color="warning" sx={{ marginLeft: "auto" }} />
-                    <Typography variant="h6" fontWeight={60} color={"orangered"} >7728988448</Typography>
-                    <>
-                        <Tabs sx={{ marginLeft: "auto" }}>
-                            <Tab label="About US" />
-                            <Tab label="Admission" />
-                            <Tab label="Gallery" />
-                            <Tab label="Facilities" />
-                        </Tabs>
-                    </>
+                    {
+                        isMatch ? (
+                            <>
+                                <Typography
+                                    fontFamily={"revert"}
+                                    color={"#121858"}
+                                    alignItems={"center"}
+                                    justifyContent={"center"}
+                                    sx={{paddingLeft:"5%", fontSize:"2rem"}}
+                                >
+                                    R K Memorial Hr. Sec. School
+                                    <Typography variant="caption" alignItems={"center"} justifyContent={"center"} display="block" color={"#121858"}>
+                                        DISE Code: 312241   Reg. No:1875
+                                    </Typography>
+                                </Typography>
+                                <DrawerComp />
+                            </>
+
+                        ) : (
+                            <>
+                                <MailOutlineIcon color="warning" sx={{ marginLeft: "auto" }} />
+                                <Typography variant="h6" fontWeight={70} color={"orangered"}> rkmemorialhss@gmail.com</Typography>
+                                <PhoneInTalkIcon color="warning" sx={{ marginLeft: "auto" }} />
+                                <Typography variant="h6" fontWeight={60} color={"orangered"} >7728988448</Typography>
+                                <Tabs sx={{ marginLeft: "auto" }} indicatorColor="secondary" value={value} onChange={(e: React.SyntheticEvent, value:stateProp) => { setValue(value) }}>
+                                    <Tab component="text" label="About Us" />
+                                    <Tab component="text" label="Admission" />
+                                    <Tab component="text" label="Gallery" />
+                                    <Tab component="text" label="Pay Fees" />
+                                </Tabs>
+                            </>
+                        )
+                    }
                 </Toolbar>
             </AppBar>
             <Carousal />
