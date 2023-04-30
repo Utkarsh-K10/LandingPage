9c2990790a8628b8f2fc1dfde3f6da76f0aad5c4
Utkarsh-K10 <utkarshkushwah9@gmail.com>
2023-04-30 17:40:30 +0530

    drawer and responsive

--- src/DrawerComp.tsx
+++ src/DrawerComp.tsx
@@ -1,25 +1,27 @@
 import React, { useState } from "react"
-import { Drawer, IconButton, List, ListItemButton, ListItemIcon, ListItemText} from "@mui/material"
+import { Drawer, IconButton, List, ListItemButton, ListItemIcon, ListItemText } from "@mui/material"
 import MenuIcon from '@mui/icons-material/Menu';
 
-export const DrawerComp:React.FC = () => {
+
+export const DrawerComp: React.FC = () => {
+  const tabspage = ["About Us", "Admission", "Pay Fee", "Notice"]
   const [opendrawer, setOpendrawer] = useState(false);
   return (
     <React.Fragment>
-      <Drawer open={opendrawer} onClose={()=>setOpendrawer(false)} >
+      <Drawer open={opendrawer} onClose={() => setOpendrawer(false)} >
         <List>
-          <ListItemButton>
-            <ListItemIcon>
-              <ListItemText>About Us</ListItemText>
-              <ListItemText>Admission Enquiry</ListItemText>
-              <ListItemText>Pay Fee</ListItemText>
-              <ListItemText>FAQs</ListItemText>
-            </ListItemIcon>
-          </ListItemButton>
+          {
+            tabspage.map((tabs, index) => (
+              <ListItemButton key={index}>
+                <ListItemIcon>
+                  <ListItemText>{tabs}</ListItemText>
+                </ListItemIcon>
+              </ListItemButton>
+            ))}
         </List>
       </Drawer>
-      <IconButton onClick={()=>setOpendrawer(!opendrawer)} sx={{color:"orangered", marginLeft:"auto"}}>
-        <MenuIcon/>
+      <IconButton onClick={() => setOpendrawer(!opendrawer)} sx={{ color: "orangered", marginLeft: "auto" }}>
+        <MenuIcon />
       </IconButton>
     </React.Fragment>
   )
--- src/Header.tsx
+++ src/Header.tsx
@@ -7,12 +7,12 @@ import PhoneInTalkIcon from '@mui/icons-material/PhoneInTalk';
 import { DrawerComp } from "./DrawerComp";
 
 interface stateProp {
-    value?:number
+    value?: number
 }
 
-export const Header:React.FC = () => {
+export const Header: React.FC = () => {
 
-    const [value, setValue] = React.useState<stateProp>({value:0})
+    const [value, setValue] = React.useState<stateProp>({ value: 0 })
     const theme = useTheme();
     const isMatch = useMediaQuery(theme.breakpoints.down("md"))
 
@@ -33,27 +33,29 @@ export const Header:React.FC = () => {
                         isMatch ? (
                             <>
                                 <Typography
+                                sx={{fontSize: "2rem",justifyContent:"center", alignItems:"center", margin:"auto"}}
+                                >
+                                    <Typography variant="caption" color={"orangered"} gutterBottom sx={{ display: { sm: "block", md: "none" , xs:"block"} }}>Admission Enquiry: 7728988448</Typography>
+                                    <Typography 
+                                    sx={{ display: { sm: "block", md: "none" , xs:"none"} }} 
                                     fontFamily={"revert"}
                                     color={"#121858"}
+                                    variant="h5"
                                     alignItems={"center"}
-                                    justifyContent={"center"}
-                                    sx={{paddingLeft:"5%", fontSize:"2rem"}}
-                                >
-                                    R K Memorial Hr. Sec. School
-                                    <Typography variant="caption" alignItems={"center"} justifyContent={"center"} display="block" color={"#121858"}>
+                                    > R K Memorial Hr. Sec. School</Typography> 
+                                    <Typography variant="caption" alignItems={"center"} justifyContent={"center"} display="block" color={"#121858"} sx={{ display: { xs: "block", sm: "block", md: "none" } }}>
                                         DISE Code: 312241   Reg. No:1875
                                     </Typography>
                                 </Typography>
                                 <DrawerComp />
                             </>
-
                         ) : (
                             <>
                                 <MailOutlineIcon color="warning" sx={{ marginLeft: "auto" }} />
                                 <Typography variant="h6" fontWeight={70} color={"orangered"}> rkmemorialhss@gmail.com</Typography>
-                                <PhoneInTalkIcon color="warning" sx={{ marginLeft: "auto" }} />
-                                <Typography variant="h6" fontWeight={60} color={"orangered"} >7728988448</Typography>
-                                <Tabs sx={{ marginLeft: "auto" }} indicatorColor="secondary" value={value} onChange={(e: React.SyntheticEvent, value:stateProp) => { setValue(value) }}>
+                                <PhoneInTalkIcon color="warning" sx={{ marginLeft: "auto" }}/>
+                                <Typography variant="body1" fontWeight={60} color={"orangered"}>7728988448</Typography>
+                                <Tabs sx={{ marginLeft: "auto" }} indicatorColor="secondary" value={value} onChange={(e: React.SyntheticEvent, value: stateProp) => { setValue(value) }}>
                                     <Tab component="text" label="About Us" />
                                     <Tab component="text" label="Admission" />
                                     <Tab component="text" label="Gallery" />
