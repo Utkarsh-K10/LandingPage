48854b312b516e0d4bb4b7212123445d65a13079
Utkarsh-K10 <utkarshkushwah9@gmail.com>
2023-05-14 00:18:37 +0530

    ne login Page

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -2,9 +2,33 @@ import { Button, Chip, Divider, Paper, Stack, TextField, Typography } from '@mui
 import React from 'react'
 import {useForm} from 'react-hook-form'
 import SupportAgentIcon from '@mui/icons-material/SupportAgent';
+import * as yup from "yup"
+import { yupResolver } from '@hookform/resolvers/yup';
 
+const schema = yup.object({ 
+    email:yup.string().required("Email Required"),
+    password: yup.string().required("enter Password")
+})
+
+type formVlaue = {
+    email: string,
+    password:string,
+}
 const LoginPage = () => {
-    const form = useForm();
+    const form = useForm<formVlaue>(({
+        defaultValues:{
+            email:"test@gmail.com",
+            password:"axy@123",
+        },
+        resolver:yupResolver(schema),
+    }));
+    const {register, handleSubmit, formState} = form
+    const {errors} = formState; 
+
+    const onSubmit = (data:formVlaue)=>{
+        console.log(data)
+    }
+
   return (
     <React.Fragment>
     <Paper elevation={4} sx={{ width: 450, margin: "auto", paddingY: 2 }}>
@@ -17,8 +41,8 @@ const LoginPage = () => {
                 <Divider variant="middle" />
                 <form onSubmit={handleSubmit(onSubmit)} noValidate>
                     <Stack spacing={2} width={400} margin={"auto"} marginTop={2}>
-                        <TextField label="email" type='email' {...register("email")} helperText={errors.email?.message} />
-                        <TextField label="mobile" type="number" {...register("mobile")} helperText={errors.mobile?.message} />
+                        <TextField label="Email" type='email' {...register("email")} helperText={errors.email?.message} />
+                        <TextField label="Password" type="password" {...register("password")} helperText={errors.password?.message} />
                         <TextField multiline maxRows={4} label="Enter Your Query" />
                         <Button type='submit' variant='outlined'>Submit</Button>
                     </Stack>
