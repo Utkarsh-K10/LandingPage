5b977c549377681fdd0f6d9ab705bacc5624c21a
Utkarsh-K10 <utkarshkushwah9@gmail.com>
2023-05-20 16:42:43 +0530

    signup form validation

--- src/components/pages/SignupPage.tsx
+++ src/components/pages/SignupPage.tsx
@@ -1,4 +1,4 @@
-import { Button, Chip, Divider, Paper, Stack, TextField, Typography } from '@mui/material'
+import { Button, Chip, Divider, FormHelperText, Paper, Stack, TextField, Typography } from '@mui/material'
 import React from 'react'
 import Face2Icon from '@mui/icons-material/Face2';
 import * as yup from "yup"
@@ -6,37 +6,36 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import { useForm } from 'react-hook-form';
 
 const schema = yup.object({
-    username:yup.string().required("Please enter username"),
-    email:yup.string().email().required("Please enter email"),
-    password:yup.string().required("enter password"),
+    username: yup.string().required("Please enter username"),
+    email: yup.string().email().required("Please enter email"),
+    password: yup.string().required("enter password"),
 });
 
 type formValue = {
-    username:string;
-    email:string;
-    password:string;
+    username: string;
+    email: string;
+    password: string;
+    confirmpassword: string;
 }
 
-const SignupPage:React.FC = () => {
+const SignupPage: React.FC = () => {
 
     const form = useForm<formValue>(({
-        defaultValues:{
-            username:"",
-            email:"",
-            password:"",
+        defaultValues: {
+            username: "",
+            email: ""
         },
-        resolver:yupResolver(schema),
+        resolver: yupResolver(schema),
     }));
-    const {register, handleSubmit, formState}  = form;
-    const {errors} = formState;
-
-    const onSubmit = (data:formValue)=>{
+    const { register, handleSubmit, formState, getValues } = form;
+    const { errors } = formState;
+    const onSubmit = (data: formValue) => {
         console.log(data);
     }
 
-  return (
-    <React.Fragment>
-        <Paper elevation={4} sx={{ width: 430, margin: "auto", paddingY: 2 }}>
+    return (
+        <React.Fragment>
+            <Paper elevation={4} sx={{ width: 430, margin: "auto", paddingY: 2 }}>
                 <Divider>
                     <Chip icon={<Face2Icon />} label="Signup" variant='outlined' color='primary' />
                 </Divider>
@@ -49,13 +48,20 @@ const SignupPage:React.FC = () => {
                         <TextField label="Username" variant='outlined' type='text' {...register("username")} helperText={errors.username?.message} />
                         <TextField label="Email" variant='outlined' type='email' {...register("email")} helperText={errors.email?.message} />
                         <TextField label="Password" variant='outlined' type='password' {...register("password")} helperText={errors.password?.message} />
-                        <TextField label="Confirm Password" variant='outlined' type='password' {...register("password")} helperText={errors.password?.message} />
+                        <TextField label="Confirm Password" variant='outlined' type='password' {...register("confirmpassword")} helperText={errors.confirmpassword?.message} />
+                        {getValues("password") && getValues("confirmpassword") && (
+                            <FormHelperText>
+                                {
+                                    getValues("password") === getValues("confirmpassword") ? (<span style={{ color: 'green' }}>password does match</span>) : (<span style={{ color: 'red' }}>password does not match</span>)
+                                }
+                            </FormHelperText>
+                        )}
                         <Button type='submit' variant='contained' disabled={!register("email") || !register("password")}>Submit</Button>
                     </Stack>
                 </form>
             </Paper>
-    </React.Fragment>
-  )
+        </React.Fragment>
+    )
 }
 
 export default SignupPage
