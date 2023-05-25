dcd3562309d7438cdb0eb49a5b3e8cc7c5a9d471
Utkarsh-K10 <utkarshkushwah9@gmail.com>
2023-05-25 15:51:21 +0530

    authentication with jwt

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -2,15 +2,20 @@
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 require('dotenv').config();
+const bcrypt = require('bcryptjs');
 
 
 exports.register = async (req, res) => {
+
+    const {username, email, password} = req.body    
+
     // check if user exists
     const usernameExists = await User.findOne({
-        username: req.body.username,
+        username,
     });
+
     const emailExists = await User.findOne({
-        email: req.body.email,
+        email,
     });
 
     if (usernameExists) {
@@ -18,82 +23,112 @@ exports.register = async (req, res) => {
             error: "username already exists",
         });
     }
+
     if (emailExists) {
         return res.status(403).json({
             error: "Email already exists",
         });
     }
+    const saltRounds = 10
+    const myEncrptPass = await bcrypt.hash( password, saltRounds)
 
     // new user registration
-    const user = new User(req.body);
-    await user.save();
-    res.status(201).json({
-        message: "registration successful! please login"
+    const user = await User.create({
+        username,
+        email,
+        hashedPassword: myEncrptPass,
     })
-
+    user.hashedPassword = undefined
+    res.status(201).json({
+        message: "registration successful! please login",
+        user
+    })
 };
 
 exports.login = async (req, res) => {
     const { email, password } = req.body;
     await User.findOne({ email })
-    .then((user) => {
-        console.log(user)
-        if (!user) {
-            return res.status(401).json({
-                error: "invalid credentials"
+        .then((user) => {
+            console.log(user)
+            if (!user) {
+                return res.status(401).json({
+                    error: "invalid credentials"
+                })
+            }
+            if (!(user.authenticate(password))) {
+                return res.status(401).json({
+                    error: "invalid email or password"
+                })
+            }
+            // jwt generation
+            const token = jwt.sign({ _id: user._id }, process.env.JSW_SECRET, { expiresIn: "24h" })
+
+            // store token in cookie
+            res.cookie("jwt", token, { expires: new Date(Date.now() + 9999), httpOnly: true })
+
+            // destructure user 
+            const { username } = user;
+
+            return res.json({
+                message: "login success",
+                username,
             })
-        }
-        if (!(user.authenticate(password))) {
-            return res.status(401).json({
-                error: "invalid email or password"
+        }).catch((error) => {
+            res.status(401).json({
+                error: error
             })
-        }
-        // jwt generation
-        const token = jwt.sign({ _id: user._id }, process.env.JSW_SECRET, { expiresIn: "24h" })
-    
-        // store token in cookie
-        res.cookie("jwt", token, { expires: new Date(Date.now() + 9999), httpOnly: true })
-    
-        // destructure user 
-        const { username } = user;
-    
-        return res.json({
-            message: "login success",
-            username,
         })
-    }).catch((error) => {
-        res.status(401).json({
-            error: error
-        })
-    })
 }
-    
-    // .exec((err, user) => {
-    //     // no user found and error then
-    //     if (err || !user) {
-    //         return res.status(401).json({
-    //             error: "invalid credentials",
-    //         });
-    //     }
-    
-    //     // if user found and authentication with form model 
-    //     if (!user.authenticate(password)) {
-    //         return res.status(401).json({
-    //             error: "invalid email or password",
-    //         })
-    //     }
-    
-    //     // jwt generation
-    //     const token = jwt.sign({ _id: user._id }, process.env.JSW_SECRET, { expiresIn: "24h" })
-    
-    //     // store token in cookie
-    //     res.cookie("jwt", token, { expires: new Date(Date.now() + 9999), httpOnly: true })
-    
-    //     // destructure user 
-    //     const { username } = user;
-    
-    //     return res.json({
-    //         message: "login success",
-    //         username,
-    //     })
-    // })
+
+// .exec((err, user) => {
+//     // no user found and error then
+//     if (err || !user) {
+//         return res.status(401).json({
+//             error: "invalid credentials",
+//         });
+//     }
+
+//     // if user found and authentication with form model 
+//     if (!user.authenticate(password)) {
+//         return res.status(401).json({
+//             error: "invalid email or password",
+//         })
+//     }
+
+//     // jwt generation
+//     const token = jwt.sign({ _id: user._id }, process.env.JSW_SECRET, { expiresIn: "24h" })
+
+//     // store token in cookie
+//     res.cookie("jwt", token, { expires: new Date(Date.now() + 9999), httpOnly: true })
+
+//     // destructure user 
+//     const { username } = user;
+
+//     return res.json({
+//         message: "login success",
+//         username,
+//     })
+// })
+// exports.login = async (req, res) => {
+//     try {
+//         const { email, password } = req.body
+
+//         if (!(email && password)) {
+//             res.status(401).json({
+//                 error: "enter valid credentials"
+//             });
+//         }
+//         const existUser = await user.findOne({ email })
+//         if(!existUser){
+//             res.status(401).json({
+//                 error:"not a registered user"
+//             });
+//         }
+        
+
+//     } catch (error) {
+//         res.status(402).json({
+//             error: error
+//         })
+//     }
+// }
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,8 +1,8 @@
 // import { Schema, model } from "mongoose";
 const mongoose = require("mongoose")
-const uuidv1 = require("uuidv1");
-const crypto = require("crypto")
 const schema = mongoose.Schema;
+const jwt = require("jsonwebtoken")
+require("dotenv")
 
 let userSchema = new schema({
     username: {
@@ -25,40 +25,60 @@ let userSchema = new schema({
         required: true
     },
 
-    salt: String
+    tokens:[{
+        token: {
+            type: String,
+            default: null
+        },
+    }]  
 },
     {
         timestamps: true,
     })
 
-userSchema.virtual("password").set(function (password) {
-    // creating temp variable password
-    this._password = password;
+// userSchema.virtual("password").set(function (password) {
+//     // creating temp variable password
+//     this._password = password;
 
-    // generate timestamp uuid gives unix timestamp
-    this.salt = uuidv1();
+//     // encrypt password function call
+//     this.hashedPassword = this.encryptPassword(password);
+//     console.log(this.hashedPassword);
 
-    // encrypt password function call
-    this.hashedPassword = this.encryptPassword(password);
+// })
+userSchema.methods.generateAuthtoken = async function(){
+    try {
+        const ttoken = jwt.sign({_id:this._id.toString()}, process.env.JWT_SCERET)
+        this.tokens = this.tokens.concate({token:ttoken})
+        await this.save()
+        return ttoken
+    } catch (error) {
+        console.log(error)
+    }
 
-})
+}
+// userSchema.methods = {
+//     encryptPassword: function (password) {
+//         if (!password)
+//             return "";
+//         else{
+//             bcrypt.hash(password, 10).then(hash =>password=hash).catch(err => { console.error(err.message) })
+//         }
+//     }
+// }
 
-userSchema.methods = {
-    encryptPassword: function (password) {
-        if (!password)
-            return "";
-        try {
-            return crypto
-                .createHmac('sha256', this.salt)
-                .update(password)
-                .digest("hex")
-        } catch (error) {
-            return "";
-        }
-    },
-    authenticate: function (plainText) {
-        this.encryptPassword(plainText) === this.hashedPassword;
-    },
-};
+
+//         try {
+//             return crypto 
+//                 .createHmac('sha256', this.salt)
+//                 .update(password)
+//                 .digest("hex")
+//         } catch (error) {
+//             return "";
+//         }
+//     },
+//     authenticate: function (plainText) {
+//         this.encryptPassword(plainText) === this.hashedPassword;
+//     },
+// };
 
 module.exports = mongoose.model('User', userSchema);
